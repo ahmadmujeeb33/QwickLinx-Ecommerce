d073df923112803cbf49297db0bdd5328491f0b2
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-03-15 19:10:39 -0400

    have product and catagories woring otgether

--- client/src/Components/SpecificCategory/hooks/useData.jsx
+++ client/src/Components/SpecificCategory/hooks/useData.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import axios from 'axios'
 
 
-export const useData = ( {id} ) => {
+export const useData = ( id ) => {
 
     const [data, setData] = useState([])
 
+    console.log("props", id)
+
     useEffect(() => {
         const grabCategoriesData = async () => {
+            console.log("id", id)
             const categoriesData = await axios.get(`/api/categories/${id}`)
             console.log("categoriesData", categoriesData)
             setData(categoriesData.data)
