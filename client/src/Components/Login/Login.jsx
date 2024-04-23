9baad424cf9e0d6d90c2f9df59a3f7115c737e80
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-04-23 13:42:18 -0400

    fixed login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -13,27 +13,45 @@ export const Login = () =>{
 
   return (
     <div className="login-container">
-      <div class="w-full max-w-xs ">
-        <form onSubmit={handleSubmit(onSubmit)}  class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      <div className="w-full max-w-xs">
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label htmlFor="userName" className="block text-gray-700 text-sm font-bold mb-2">
               Username
             </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+            <input 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+              id="userName" 
+              type="text" 
+              placeholder="Username" 
+              {...register('userName', { required: true })} 
+            />
+            { errors.userName  && <p style={{ color: 'red' }}>{errors.userName.message}</p>}
           </div>
-          <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
               Password
             </label>
-            <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+            <input 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+              id="password" 
+              type="password" 
+              placeholder="******************" 
+              {...register('password', { required: true })} 
+            />
+            { errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
           </div>
-          <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-             Login
+          <div className="flex items-center justify-between">
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+              type="submit"
+            >
+              Login
             </button>
           </div>
+        <p style={{ color: 'red'}} className="pt-3.5">{error}</p>
         </form>
       </div>
     </div>
-  )
-  }
+  );
+}
--- client/src/Components/Login/hooks/useLoginUser.jsx
+++ client/src/Components/Login/hooks/useLoginUser.jsx
@@ -3,7 +3,11 @@ import Auth from '../../../utils/auth';
 
 
 export const useLoginUser = (setError)=>{
+
+    console.log("in login")
+
     const onSubmit = async(data) =>{
+        console.log("data", data)
         try {
             const userData = await axios.post("/api/users/login",data)
             Auth.login(userData.data.token)
